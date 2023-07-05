7a5ce10c0997a6d54abc8de1c44496bf14d9649e
HtetAungLinn98 <92046483+htetaunglinn1998@users.noreply.github.com>
2023-07-05 14:32:57 +0630

    tsconfigpaths added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
-import logo from '../../assets/logo.png'
+import logo from 'assets/logo.png'
+
 
 const Navbar = () => {
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import {visualizer} from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    visualizer({
+      open:true,
+      gzipSize:true,
+      brotliSize:true,
+    })
+  ],
 })
