12deeb3172606f7f75b33964e71c435f293393c8
HtetAungLinn98 <92046483+htetaunglinn1998@users.noreply.github.com>
2023-07-05 17:35:56 +0630

    roboto font added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,8 @@ import hero_background from 'assets/background.png'
 const Home = () => {
   return (
     <div className='bg-primary'>
-      <section className='relative '>
-      <img src={hero_background} alt="hero-background" className='h-100vh w-[90%]'/>
+      <section className='relative'>
+      <img src={hero_background} alt="hero-background" className='h-100vh w-[85%] object-fit'/>
       </section>
     </div>
   )
