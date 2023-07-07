8a0f33dc7419408434437ea7dc8eb0e90b194986
HtetAungLinn98 <92046483+htetaunglinn1998@users.noreply.github.com>
2023-07-07 14:03:36 +0630

    fixed animation

--- src/pages/BusinessSection.tsx
+++ src/pages/BusinessSection.tsx
@@ -23,7 +23,7 @@ const BusinessSection = () => {
         </Fade>
 
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 '>
-          <Fade direction='up' fraction={0}>
+          <Fade direction='up'>
             {stocks.map(data => (
               <div
                 key={data.img}
