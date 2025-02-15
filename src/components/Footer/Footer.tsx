2805e3e2e9ee2ead1894db9cc14e3a0c5798490a
HtetAungLinn-Go5 <htet.l@gofive.co.th>
2025-02-15 23:29:12 +0700

    update(): phone number and logo

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,53 +1,53 @@
-import logo from 'assets/ENCO.svg'
-import { ImLinkedin } from 'react-icons/im'
-import { FaSquareTwitter, FaSquareWhatsapp } from 'react-icons/fa6'
-import { FaFacebookSquare } from 'react-icons/fa'
-import { Fade, Slide } from 'react-awesome-reveal'
+import logo from "assets/global.svg";
+import { ImLinkedin } from "react-icons/im";
+import { FaSquareTwitter, FaSquareWhatsapp } from "react-icons/fa6";
+import { FaFacebookSquare } from "react-icons/fa";
+import { Fade, Slide } from "react-awesome-reveal";
 
 const Footer = () => {
   return (
-    <div className=' w-full py-32 bg-milk overflow-hidden'>
-      <div className='ml-[9.5%] w-[82%] '>
-        <section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5'>
+    <div className=" w-full py-32 bg-milk overflow-hidden">
+      <div className="ml-[9.5%] w-[82%] ">
+        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
           {/* Logo */}
 
-          <div className='flex justify-center items-center'>
-            <Slide direction='left'>
-              <img src={logo} alt='logo' className='w-36' />
+          <div className="flex justify-center items-center">
+            <Slide direction="left">
+              <img src={logo} alt="logo" className="w-36" />
             </Slide>
           </div>
 
           {/* Navlinks */}
           <div>
-            <Fade direction='up'>
-              <ul className='flex flex-col space-y-4 mt-10'>
+            <Fade direction="up">
+              <ul className="flex flex-col space-y-4 mt-10">
                 <li>
-                  <a href='#' className='link'>
+                  <a href="#" className="link">
                     About Us
                   </a>
                 </li>
                 <li>
-                  <a href='#' className='link'>
+                  <a href="#" className="link">
                     Products and services
                   </a>
                 </li>
                 <li>
-                  <a href='#' className='link'>
+                  <a href="#" className="link">
                     Manufacturing process
                   </a>
                 </li>
                 <li>
-                  <a href='#' className='link'>
+                  <a href="#" className="link">
                     Research Development
                   </a>
                 </li>
                 <li>
-                  <a href='#' className='link'>
+                  <a href="#" className="link">
                     Case Study
                   </a>
                 </li>
                 <li>
-                  <a href='#' className='link'>
+                  <a href="#" className="link">
                     News
                   </a>
                 </li>
@@ -56,66 +56,66 @@ const Footer = () => {
           </div>
 
           {/* Contacts */}
-          <div className='mt-10'>
-            <Fade direction='up'>
-              <a href='https://wa.me/6588105141' className='text-2xl'>
+          <div className="mt-10">
+            <Fade direction="up">
+              <a href="https://wa.me/6588105141" className="text-2xl">
                 +65 8810 5141
               </a>
             </Fade>
 
-            <div className='text-sm my-5'>
-              <Fade delay={0.3} direction='up' cascade>
+            <div className="text-sm my-5">
+              <Fade delay={0.3} direction="up" cascade>
                 <p>lavender street</p>
                 <p>Singapore</p>
                 <p>DWTX GPR</p>
               </Fade>
             </div>
 
-            <div className='flex gap-3'>
-              <Fade delay={0.5} direction='up'>
-                <a href='#'>
+            <div className="flex gap-3">
+              <Fade delay={0.5} direction="up">
+                <a href="#">
                   <FaFacebookSquare />
                 </a>
-                <a href='#'>
+                <a href="#">
                   <FaSquareWhatsapp />
                 </a>
-                <a href='#'>
+                <a href="#">
                   <FaSquareTwitter />
                 </a>
-                <a href='#'>
+                <a href="#">
                   <ImLinkedin />
                 </a>
               </Fade>
             </div>
           </div>
         </section>
-        <hr className='my-5' />
+        <hr className="my-5" />
         <section>
-          <div className='flex flex-wrap gap-3'>
-            <Fade direction='up' cascade>
-              <a href='#' className='link'>
+          <div className="flex flex-wrap gap-3">
+            <Fade direction="up" cascade>
+              <a href="#" className="link">
                 Contact US
               </a>
-              <a href='#' className='link'>
+              <a href="#" className="link">
                 Term of use
               </a>
-              <a href='#' className='link'>
+              <a href="#" className="link">
                 Privacy Cookie
               </a>
-              <a href='#' className='link'>
+              <a href="#" className="link">
                 Policy
               </a>
             </Fade>
           </div>
-          <div className='text-sm opacity-60 mt-3'>
-            <Fade direction='up' delay={1} duration={2000}>
+          <div className="text-sm opacity-60 mt-3">
+            <Fade direction="up" delay={1} duration={2000}>
               &copy; 2023 - All rights reserved. Powered by Dev Genius
             </Fade>
           </div>
         </section>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,32 +1,35 @@
-import logo from 'assets/global.svg'
-import Drawer from 'components/Drawer/Drawer'
-import { useState } from 'react'
-import { RxHamburgerMenu } from 'react-icons/rx'
+import logo from "assets/global.svg";
+import Drawer from "components/Drawer/Drawer";
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
-    <nav className='absolute z-50 top-0 right-0 lg:right-[5%] left-0 pl-5 pr-10 py-3 bg-white/20 backdrop:blur-lg select-none'>
-      <div className='flex justify-between items-center'>
-        <img src={logo} alt='company logo' className='w-20 ' />
+    <nav className="absolute z-50 top-0 right-0 lg:right-[5%] left-0 pl-5 pr-10 py-3 bg-white/20 backdrop:blur-lg select-none">
+      <div className="flex justify-between items-center">
+        <img src={logo} alt="company logo" className="w-20 " />
 
-        <section className='flex items-center gap-3 text-black'>
-          <a href='https://wa.me/6588105141' className='hidden lg:inline-block'>
-            +65 8810 5141
+        <section className="flex items-center gap-3 text-black">
+          <a
+            href="https://wa.me/66804642352"
+            className="hidden lg:inline-block"
+          >
+            +66 804642352
           </a>
           <a
-            href='https://wa.me/6588105141'
-            className='hidden lg:inline-block rounded border border-black/40 py-2 px-6 hover:bg-black/80 hover:text-white transition-all duration-300 ease-out'
+            href="https://wa.me/66804642352"
+            className="hidden lg:inline-block rounded border border-black/40 py-2 px-6 hover:bg-black/80 hover:text-white transition-all duration-300 ease-out"
           >
             Contact Us
           </a>
 
-          <div className='lg:hidden'>
+          <div className="lg:hidden">
             <button
               onClick={() => setIsOpen(true)}
-              className='border border-black/40 rounded-md p-1 hover:scale-105'
-              aria-label='Menu Button'
+              className="border border-black/40 rounded-md p-1 hover:scale-105"
+              aria-label="Menu Button"
             >
               <RxHamburgerMenu />
             </button>
@@ -37,42 +40,42 @@ const Navbar = () => {
       {/* Drawer */}
       <Drawer isOpen={isOpen} setIsOpen={setIsOpen} />
 
-      <div className=' hidden lg:block'>
-        <ul className='flex gap-5 text-black font-semi-bold tracking-wide'>
+      <div className=" hidden lg:block">
+        <ul className="flex gap-5 text-black font-semi-bold tracking-wide">
           <li>
-            <a href='#' className='link'>
+            <a href="#" className="link">
               About Us
             </a>
           </li>
           <li>
-            <a href='#' className='link'>
+            <a href="#" className="link">
               Products and services
             </a>
           </li>
           <li>
-            <a href='#' className='link'>
+            <a href="#" className="link">
               Manufacturing process
             </a>
           </li>
           <li>
-            <a href='#' className='link'>
+            <a href="#" className="link">
               Research Development
             </a>
           </li>
           <li>
-            <a href='#' className='link'>
+            <a href="#" className="link">
               Case Study
             </a>
           </li>
           <li>
-            <a href='#' className='link'>
+            <a href="#" className="link">
               News
             </a>
           </li>
         </ul>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
